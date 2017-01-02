d5e9b41ac9fc56c9638bd3692168dc8e14b6cdf4
cronustimeoff <cronustimeoff@gmail.com>
2017-01-01 20:56:41 -0600

    WebService

--- soapWebServices/src/soapWebServices/ProductCatalog.java
+++ soapWebServices/src/soapWebServices/ProductCatalog.java
@@ -3,8 +3,13 @@ package soapWebServices;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+@WebService
 public class ProductCatalog {
 
+	@WebMethod
 	public List<String> getProductsCategory() {
 		List<String> categorys = new ArrayList<>();
 		categorys.add("Books");
